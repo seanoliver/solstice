62c5b37f104b07b8e0cdaf9bc6f2ed595b0a9b0d
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2026-05-18 09:53:43 -0700

    feat: timezone config list

---
+++ config.js
@@ -0,0 +1,9 @@
+export const ZONES = [
+  { label: "You",       tz: "local" },
+  { label: "SF/LA",     tz: "America/Los_Angeles" },
+  { label: "London",    tz: "Europe/London" },
+  { label: "Berlin",    tz: "Europe/Berlin" },
+  { label: "Singapore", tz: "Asia/Singapore" },
+  { label: "Beijing",   tz: "Asia/Shanghai" },
+  { label: "Sydney",    tz: "Australia/Sydney" },
+];
